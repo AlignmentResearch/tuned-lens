58a519a0dd56be79cd1e84a2e896057874cb18c8
loganriggs <logan.smith.5@gmail.com>
2022-12-01 12:53:02 -0500

    Added rank, topk, and testing for plotting

--- white_box/plotting.py
+++ white_box/plotting.py
@@ -34,12 +34,18 @@ def plot_logit_lens(
     tuned_lens: Optional[TunedLens] = None,
     tokenizer: Optional[PreTrainedTokenizerBase] = None,
     rank: int = 0,
-    top_k: int = 0,
-    top_k_diff: bool = False,
+    topk: int = 5,
+    topk_diff: bool = False,
     whitespace_token: str = "Ġ",
-    whitespace_replacement: str = " ",
+    whitespace_replacement: str = "_",
+    newline_token: str = "Ċ",
+    newline_replacement: str = "\\n",
     add_last_tuned_lens_layer: bool = False,
+    test_text: str = None,
 ):
+    if(topk<1):
+        raise ValueError("topk must be greater than 0")
+
     """Plot a logit lens table for the given text."""
     model, tokens, outputs, stream = _run_inference(
         model_or_name, input_ids, text, tokenizer, sublayers, start_pos, end_pos
@@ -72,35 +78,30 @@ def plot_logit_lens(
 
         hidden_lps = stream.map(decode_fn)
 
-    reversed_colors = False #Only true for probs
-    if rank > 0:
-        #token_offset is the number of tokens predicted ahead. 1 means the next token, 2 means the token after that, etc.
+    #If rank, get the rank of next token predicted
+    #Set this to the stats & top_strings
+    if(rank > 0):
+        #remove the last rank sequences from the stream
         token_offset = rank
-        #Skip the first token because not predicted
-        next_token_ids = input_ids[:, token_offset:] 
-        #Remove the last token seq from hidden_lps because it is not predicted
         hidden_lps = hidden_lps.map(lambda x: x[:, :-token_offset, :])
-        #Get the sorted indices of the logits for each layer of hidden_lps
-        sorted = hidden_lps.map(lambda x: x.argsort(dim=-1, descending=True))
-        #Get the rank of the ground-truth next token for each layer
-        ranks = sorted.map(lambda x: (next_token_ids.unsqueeze(-1) == x).nonzero()[:, -1])
-        #Convert hidden_lps to probabilities
-        hidden_lps = hidden_lps.map(lambda x: x.exp()) 
-        #Get the prob of the ground-truth next token for each layer
-        stats = hidden_lps.map(lambda x: x.gather(-1, next_token_ids.unsqueeze(-1))) 
-        
-        top_strings = ranks #TODO change to "display" variable instead of top_strings
-        max_color = 1.0
-        metric = "probs"
-        reversed_colors = True
+        ranks = _get_rank(hidden_lps, token_offset, input_ids)
+        top_strings = ranks
+        # stats = ranks
+        stats = ranks
+        stats = stats.map(lambda x: th.log10(x + 1))
+        metric = "rank"
+        max_color = int(np.log10(10000)) #out of 50k tokens
+        tokens = tokens[:-token_offset]
+        colorbar_label = "Rank"
+        colorscale = "blues"
     else:
-
         top_tokens = hidden_lps.map(lambda x: x.argmax(-1).squeeze().cpu().tolist())
         top_strings = top_tokens.map(
             tokenizer.convert_ids_to_tokens  # type: ignore[arg-type]
         )
+        #Replace whitespace and newline tokens with their respective replacements
         top_strings = top_strings.map(
-            lambda x: [t.replace(whitespace_token, whitespace_replacement) for t in x]
+            lambda x: [t.replace(whitespace_token, whitespace_replacement) if whitespace_token in t else t.replace(newline_token, newline_replacement) if newline_token in t else t for t in x]
         )
         max_color = math.log(model.config.vocab_size)
         if metric == "ce":
@@ -122,94 +123,97 @@ def plot_logit_lens(
             )
         else:
             raise ValueError(f"Unknown metric: {metric}")
+        colorbar_label = f"{metric} (nats)"
+        colorscale = "rdbu_r"
 
-    if(top_k > 0): #TODO make title tuned_lens
-        probs = hidden_lps.map(lambda x: x.exp()*100)
-        return plot_topk_probs(
-            stats.map(lambda x: x.squeeze().cpu()),
-            probs,
-            [t.replace(whitespace_token, whitespace_replacement) for t in tokens],
-            tokenizer=tokenizer,
-            colorbar_label=f"{metric} (nats)",
-            fmt="",
-            layer_stride=layer_stride,
-            vmax=max_color,
-            k=top_k,
-            top_k_diff=top_k_diff,
-            whitespace_token = whitespace_token,
-            whitespace_replacement=whitespace_replacement,
-        )
-    else:
-        _plot_stream(
-            stats.map(lambda x: x.squeeze().cpu()),
-            top_strings,
-            [t.replace(whitespace_token, whitespace_replacement) for t in tokens],
-            colorbar_label=f"{metric} (nats)",
-            fmt="",
-            layer_stride=layer_stride,
-            vmax=max_color,
-            reversed_colors=reversed_colors,
-        )
+    x_labels =  [t.replace(whitespace_token, whitespace_replacement) if whitespace_token in t else t.replace(newline_token, newline_replacement) if newline_token in t else t for t in tokens]
+    topk_strings_and_probs = _get_topk_probs(hidden_lps=hidden_lps, tokenizer=tokenizer, k=topk, topk_diff=topk_diff)
+    topk_strings_and_probs = np.array([[[[t.replace(whitespace_token, whitespace_replacement) if whitespace_token in t else t.replace(newline_token, "\\n") if newline_token in t else t for t in x] for x in y] for y in z] for z in topk_strings_and_probs])
 
-        name = "Logit" if tuned_lens is None else "Tuned"
-        plt.title(f"{name} lens ({model.name_or_path})")
+    _plot_stream( #TODO, do we have to return?
+        color_stream = stats.map(lambda x: x.squeeze().cpu()),
+        top_k_strings_and_probs = topk_strings_and_probs,
+        x_labels = x_labels,
+        top_1_strings = top_strings,
+        colorbar_label = colorbar_label,
+        layer_stride = 1,
+        vmax = max_color,
+        k = topk,
+        title= ("Tuned Lens" if tuned_lens is not None else "Logit Lens") + (": Top Token" if rank < 1 else f": Rank {rank}") + ("[Test: " + test_text + "]" if test_text else ""),
+        colorscale = "rdbu_r" if rank < 1 else "blues",
+        rank = rank,
+    )
 
-def plot_topk_probs(
-    color_stream: ResidualStream,
-    text_stream: ResidualStream,
-    x_labels: Sequence[str] = (),
-    tokenizer: Optional[PreTrainedTokenizerBase] = None,
-    colorbar_label: str = "",
-    fmt: str = "0.2f",
-    layer_stride: int = 1,
-    vmax=None,
-    k=5,
-    top_k_diff=False,
-    whitespace_token: str = "Ġ",
-    whitespace_replacement: str = " ",
-
+def _get_rank(
+    stream: ResidualStream,
+    token_offset: int=1,
+    input_ids: th.Tensor=None,
 ):
-    if(top_k_diff):
-        top_tokens = text_stream.map(lambda x: x.argmax(-1).squeeze().cpu().tolist())
-        top_1_strings = top_tokens.map(
-            tokenizer.convert_ids_to_tokens  # type: ignore[arg-type]
-        )
-        top_1_strings = np.stack(list(top_1_strings))[::layer_stride]
-        text_stream = text_stream.pairwise_map(lambda x, y: y - x)
+    #Skip the first token because not predicted
+    next_token_ids = input_ids[:, token_offset:] 
+    #Get the sorted indices of the logits for each layer of hidden_lps
+    stream = stream.map(lambda x: x.argsort(dim=-1, descending=True))
+    #Get the rank of the ground-truth next token for each layer
+    return stream.map(lambda x: (next_token_ids.unsqueeze(-1) == x).nonzero()[:, -1])
 
-    color_matrix = np.stack(list(color_stream))[::layer_stride]
-    probs = th.stack(list(text_stream))[::layer_stride].squeeze(1)
-    y_labels = color_stream.labels()
-    x_labels = [x + "\u200c"*i for i, x in enumerate(x_labels)]
 
-    if(top_k_diff):
+def _get_topk_probs(
+    hidden_lps: ResidualStream,
+    tokenizer: Optional[PreTrainedTokenizerBase] = None,
+    k=5,
+    topk_diff=False,
+):  
+    probs = hidden_lps.map(lambda x: x.exp()*100)
+    if(topk_diff):
+        probs = probs.pairwise_map(lambda x, y: y - x)
+    probs = th.stack(list(probs)).squeeze(1)
+
+    if(topk_diff):
         topk = probs.abs().topk(k, dim=-1)
         topk_values = probs.gather(-1, topk.indices) #get the topk values but include negative values
-        #TODO custom hovertemplate for green text up and red text down
     else:
         #Get the top-k tokens & probabilities for each
         topk = probs.topk(k, dim=-1)
         topk_values = topk.values
-    #reshape top_k_ind from (layers, seq, k) to (layers*seq*k), convert_ids_to_tokens, then reshape back to (layers, seq, k)
-    top_k_ind = tokenizer.convert_ids_to_tokens(topk.indices.reshape(-1).tolist())
-    top_k_ind = np.array(top_k_ind).reshape(topk.indices.shape)
-    # top_k_ind = [t.replace(whitespace_token, whitespace_replacement) for t in top_k_ind]
+    #reshape topk_ind from (layers, seq, k) to (layers*seq*k), convert_ids_to_tokens, then reshape back to (layers, seq, k)
+    topk_ind = tokenizer.convert_ids_to_tokens(topk.indices.reshape(-1).tolist())
+    topk_ind = np.array(topk_ind).reshape(topk.indices.shape)
 
-#Grab the top 1 strings for displaying on Z axis
-    if not top_k_diff:
-        top_1_strings = top_k_ind[:,:,0]
-    
-    #replace text for top_k_ind & top_1_strings
-    newline_token = "Ċ"
-    top_k_ind = np.array([[[t.replace(whitespace_token, whitespace_replacement) if whitespace_token in t else t.replace(newline_token, "\\n") if newline_token in t else t for t in x] for x in y] for y in top_k_ind])
-    top_1_strings = np.array([[t.replace(whitespace_token, whitespace_replacement) if whitespace_token in t else t.replace(newline_token, "\\n") if newline_token in t else t for t in x] for x in top_1_strings])
-    
-    top_k_strings_and_probs = np.stack((top_k_ind, topk_values.numpy()), axis=-1)
-    if top_k_diff:
-        #add a new bottom row of "N/A" for top_k_strings_and_probs
-        top_k_strings_and_probs = np.concatenate((np.full((1, top_k_strings_and_probs.shape[1], k, 2), "N/A"), top_k_strings_and_probs), axis=0)
-    #return all the variables used in the figure below
-    # return color_matrix, top_k_strings_and_probs, top_1_strings, y_labels, x_labels, colorbar_label, fmt, layer_stride, vmax, k
+    topk_strings_and_probs = np.stack((topk_ind, topk_values.numpy()), axis=-1)
+    if topk_diff:
+        #add a new bottom row of "N/A" for topk_strings_and_probs because we don't have a "previous" layer to compare to
+        topk_strings_and_probs = np.concatenate((np.full((1, topk_strings_and_probs.shape[1], k, 2), "N/A"), topk_strings_and_probs), axis=0)
+
+    return topk_strings_and_probs
+
+def _plot_stream(
+    color_stream: ResidualStream,
+    top_k_strings_and_probs = None, #TODO np array?,
+    x_labels: Sequence[str] = (),
+    top_1_strings = None, # TODO np array?
+    colorbar_label: str = "",
+    layer_stride: int = 1,
+    vmax: int = None,
+    k: int =5,
+    title: str="",
+    colorscale: str="rdbu_r",
+    rank: int = 0,
+):
+
+    color_matrix = np.stack(list(color_stream))[::layer_stride]
+    top_1_strings = np.stack(list(top_1_strings))
+    y_labels = color_stream.labels()
+    x_labels = [x + "\u200c"*i for i, x in enumerate(x_labels)]
+
+    colorbar=dict(
+        title=colorbar_label, 
+        titleside="right", 
+        tickfont=dict(size=20),
+    )
+    if rank>0: #make log-scale for rank plots
+        colorbar["tickvals"] = [0, 1, 2, 3, 4]
+        colorbar["ticktext"] = ['1', '10', '100', '1000', '10000']
+
     fig = go.Figure()
     fig.add_trace(go.Heatmap(
         z=color_matrix,
@@ -219,14 +223,30 @@ def plot_topk_probs(
         textfont={"size":12},
         customdata = top_k_strings_and_probs,
         y=y_labels,
-        colorscale='rdbu_r', #or "rdbu"
+        colorscale=colorscale, #or "rdbu"
         hoverlabel=dict(bgcolor="white", font_size=20, bordercolor="black"),
         hovertemplate = '<br>'.join(f' %{{customdata[{i}][0]}}: %{{customdata[{i}][1]:.1f}}% ' for i in range(k))+ "<extra></extra>",
-        colorbar=dict(title=colorbar_label, titleside="right", tickfont=dict(size=20)),
+        colorbar=dict(
+            title=colorbar_label, 
+            titleside="right", 
+            tickfont=dict(size=20),
+            ),
         zmax=vmax,
         zmin=0,
     ))
-    fig.update_layout(title_text=f'Top {k} Logit Probabilities for Each Token', title_x=0.5, title_font_size=30, width=200+80*len(x_labels))
+
+
+    #TODO Height needs to equal some function of Max(num_layers, topk). Ignore for now. Works until k=18
+    fig.update_layout(
+        title_text=title, 
+        title_x=0.5, 
+        title_font_size=30, 
+        width=200+80*len(x_labels), 
+        xaxis_title = "Token Sequence Input",
+        yaxis_title = "Layer",
+        xaxis_title_font_size=24, yaxis_title_font_size=24,
+    )
+    
     fig.show()
 
 
@@ -343,31 +363,3 @@ def _run_inference(
 
     return model, tokens, outputs, stream
 
-
-def _plot_stream(
-    color_stream: ResidualStream,
-    text_stream: ResidualStream,
-    x_labels: Sequence[str] = (),
-    colorbar_label: str = "",
-    fmt: str = "0.2f",
-    layer_stride: int = 1,
-    vmax=None,
-    reversed_colors: bool = False,
-
-):
-    color_matrix = np.stack(list(color_stream))[::layer_stride]
-    text_matrix = np.stack(list(text_stream))[::layer_stride]
-    y_labels = color_stream.labels()
-
-    plt.subplots(figsize=(2 * len(x_labels), len(color_stream) // (2 * layer_stride)))
-    sns.heatmap(
-        np.flipud(color_matrix),
-        annot=np.flipud(text_matrix),
-        cbar_kws={"label": colorbar_label} if colorbar_label else None,
-        cmap=sns.color_palette("coolwarm_r" if reversed_colors else "coolwarm", as_cmap=True),
-        fmt=fmt,
-        robust=True,
-        vmax=vmax,
-        xticklabels=x_labels,  # type: ignore[arg-type]
-        yticklabels=y_labels[::-layer_stride],  # type: ignore[arg-type]
-    )
