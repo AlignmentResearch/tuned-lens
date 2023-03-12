947669021a2be718e51173bb7c9d898132eb9e7b
Lev McKinney <levmckinney@gmail.com>
2023-03-12 01:50:33 -0500

    fixed lint errors

--- tuned_lens/nn/downstream_wrapper.py
+++ tuned_lens/nn/downstream_wrapper.py
@@ -1,7 +1,7 @@
 from .tuned_lens import TunedLens
 from ..utils import pytree_map
 from transformers import PreTrainedModel, PreTrainedTokenizerBase
-from typing import Iterable, NamedTuple, Optional, Sequence
+from typing import Iterable, NamedTuple, Optional
 import torch as th
 import torch.distributed as dist
 
