ad41869d7a3ca73fd28715f9890bcd9ff8975909
Lev McKinney <levmckinney@gmail.com>
2023-04-27 15:51:35 -0400

    fixed __init__.py

--- tuned_lens/stats/__init__.py
+++ tuned_lens/stats/__init__.py
@@ -1,15 +1,9 @@
 from .anomaly import fit_anomaly_detector
 from .dimensionality import effective_rank
 from .distance import (
-    aitchison,
-    aitchison_similarity,
-    gaussian_wasserstein_l2,
-    gaussian_wasserstein_l2_origin,
-    geodesic_distance,
     js_divergence,
     js_distance,
     kl_divergence,
 )
 from .logit_stats import LogitStats
-from .rank import spearmanr
 from .residual_stats import ResidualStats
