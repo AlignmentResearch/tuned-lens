96380df0d173d406df55e17bbe9ead46ef6193b7
Lev McKinney <levmckinney@gmail.com>
2023-05-06 23:55:32 -0400

    Evaluating all layers not just the final one. Removed logit stats and tranfer

--- tuned_lens/scripts/eval_loop.py
+++ tuned_lens/scripts/eval_loop.py
@@ -1,4 +1,5 @@
 """Evaluation loop for the tuned lens model."""
+import re
 from collections import defaultdict
 from dataclasses import dataclass
 from itertools import islice
@@ -7,9 +8,11 @@ from typing import Optional
 
 import numpy as np
 import torch as th
+from flatten_dict import flatten
 from simple_parsing import field
 from torch.utils.data import DataLoader
 from tqdm.auto import tqdm
+from transformers import AutoModelForCausalLM, PreTrainedModel
 
 from tuned_lens.nn.lenses import Lens, LogitLens, TunedLens
 from tuned_lens.scripts.ingredients import (
@@ -17,7 +20,6 @@ from tuned_lens.scripts.ingredients import (
     Distributed,
     Model,
 )
-from tuned_lens.stats import LogitStats
 from tuned_lens.utils import (
     maybe_all_reduce,
     pytree_map,
@@ -37,20 +39,20 @@ class Eval:
 
     dist: Distributed
 
-    lens: Optional[str] = field(alias=["-l"], default=None)
+    lens_name: str = field(alias=["-l"])
     """Path to the tuned lens model."""
+
+    output: Path = field(alias=["-o"])
+    """Folder to save the eval results to."""
+
+    lens_types: list[str] = field(default_factory=lambda: ["tuned", "logit"])
+    """Types of lenses to evaluate."""
 
     seed: int = 42
     """Random seed used for data shuffling."""
 
     limit: Optional[int] = None
     """Number of batches to evaluate on. If None, will use the entire dataset."""
-
-    output: Optional[Path] = field(alias=["-o"], default=None)
-    """JSON file to save the eval results to."""
-
-    transfer: Optional[bool] = field(action="store_true")
-    """Evaluate how well probes transfer to other layers."""
 
     token_shift: Optional[int] = None
     """How to shift the labels wrt the input tokens (1 = next token, 0 = current token,
@@ -59,12 +61,29 @@ class Eval:
     per_gpu_batch_size: int = 1
     """Number of samples to try to fit on a GPU at once."""
 
-    def load_lens(self, model) -> Lens:
+    def load_lens(self, model: PreTrainedModel) -> dict[str, Lens]:
         """Load the tuned lens model."""
-        if self.lens is None:
-            return LogitLens.from_model(model)
-        else:
-            return TunedLens.from_model_and_pretrained(model, self.lens)
+        lenses = {}
+        for lens_type in self.lens_types:
+            if lens_type == "logit":
+                lenses["logit"] = LogitLens.from_model(model)
+            elif lens_type == "tuned":
+                lenses[
+                    f"tuned[{model.config.name_or_path}]"
+                ] = TunedLens.from_model_and_pretrained(model, self.lens_name)
+            elif match := re.match(r"tuned\[([a-z0-9\-]+)\]", lens_type):
+                model_name = match.group(1)
+                new_model = AutoModelForCausalLM.from_pretrained(  # type: ignore
+                    model_name,
+                    low_cpu_mem_usage=True,
+                    torch_dtype="auto",
+                )
+                lenses[f"tuned[{model_name}]"] = TunedLens.from_model_and_pretrained(
+                    new_model, self.lens_name
+                )
+            else:
+                raise ValueError(f"Unknown lens type: {lens_type}")
+        return lenses
 
     @th.autocast("cuda", enabled=th.cuda.is_available())
     @th.no_grad()
@@ -72,12 +91,12 @@ class Eval:
         """Trains a TunedLens model against a transformer on a dataset."""
         # Load model, tokenizer, data, and lens
         self.dist.init()
-        model = tokenizer = data = lens = nats_to_bpb = model_name = None
+        model = tokenizer = data = lenses = nats_to_bpb = None
         if self.dist.primary:
             # Let the primary processes populate the cache
             model, tokenizer = self.model.load()
             data, nats_to_bpb = self.data.load(tokenizer)
-            lens = self.load_lens(model)
+            lenses = self.load_lens(model)
 
         self.dist.barrier()  # Wait for primary to finish filling the cache
 
@@ -85,14 +104,14 @@ class Eval:
             # Let the non-primary processes load from the cache
             model, tokenizer = self.model.load(must_use_cache=True)
             data, nats_to_bpb = self.data.load(tokenizer)
-            lens = self.load_lens(model)
+            lenses = self.load_lens(model)
 
-        assert model and tokenizer and data and lens and nats_to_bpb
+        assert model and tokenizer and data and lenses and nats_to_bpb
 
         model = self.dist.shard_model(model)
         # Note since we are not training we can just move the lens to the device.
         # No need to use DDP
-        lens = lens.to(self.dist.device)
+        lenses = {name: lens.to(self.dist.device) for name, lens in lenses.items()}
         data = self.dist.shard_dataset(data)
 
         dl = DataLoader(
@@ -100,7 +119,8 @@ class Eval:
             batch_size=self.per_gpu_batch_size,
         )
 
-        lens.eval()
+        for lens in lenses.values():
+            lens.eval()
 
         if self.limit:
             dl = islice(dl, self.limit)
@@ -108,24 +128,13 @@ class Eval:
         else:
             total = len(dl)
 
-        *_, model_name = model.config.name_or_path.split("/")
-
-        if self.lens:
-            root_dir = Path(self.lens, "tuned-lens-eval")
-        else:
-            root_dir = Path(model_name, "logit_lens_eval")
-
-        if self.output is not None:
-            root_dir = self.output
+        root_dir = self.output
 
         root_dir.mkdir(exist_ok=True, parents=True)
 
         L = model.config.num_hidden_layers
         batches = []
-        transfer_batches = []
 
-        final_logit_stats = LogitStats()
-        lens_statistics = [LogitStats() for _ in range(L)]
         self.dist.barrier()
         print(f"All processes initialized. Running evaluation on {total} batches.")
 
@@ -135,50 +144,41 @@ class Eval:
             with th.no_grad():
                 output = model(**batch, output_hidden_states=True)
 
-            hidden_states = output.hidden_states[-1:]
+            hidden_states = output.hidden_states[:-1]
 
             shift = self.token_shift if self.token_shift is not None else 1
             final_lps = output.logits.log_softmax(dim=-1)
             final_probs = final_lps.exp()
+            assert not th.isnan(output.logits).any(), "Logits are NaN"
+
             labels = shift_labels(batch["input_ids"], shift)
 
-            batch_output = defaultdict(dict)
-            transfer_ces = th.zeros(L, L, device=final_lps.device)
-            transfer_kls = th.zeros(L, L, device=final_lps.device)
+            def nested_dict():
+                return defaultdict(nested_dict)
+
+            batch_output = nested_dict()
 
             # Compute tuned lens eval and statistics if applicable
             for j, h in zip(range(L), hidden_states):
                 name = f"layer_{j}"
-                lens_lps = lens(h, idx=j).log_softmax(dim=-1)
-                lens_probs = lens_lps.exp()
+                for lens_type, lens in lenses.items():
+                    lens_lps = lens(h, idx=j).log_softmax(dim=-1)
+                    lens_probs = lens_lps.exp()
 
-                batch_output["lens_ce"][name] = th.nn.functional.cross_entropy(
-                    shift_preds(lens_lps, shift).flatten(0, 1),
-                    labels.flatten(),
-                    reduction="none",
-                )
-                batch_output["lens_entropy"][name] = th.sum(
-                    -lens_probs * lens_lps, dim=-1
-                )
-                batch_output["lens_kl"][name] = th.sum(
-                    final_probs * (final_lps - lens_lps), dim=-1
-                )
-                lens_statistics[j].update(lens_lps, assume_normalized=True)
+                    batch_output[lens_type]["ce"][
+                        name
+                    ] = th.nn.functional.cross_entropy(
+                        shift_preds(lens_lps, shift).flatten(0, 1),
+                        labels.flatten(),
+                        reduction="none",
+                    )
 
-                if self.transfer:
-                    # Each iteration of the loop processes a different *probe*
-                    # layer i for the test layer j.
-                    for i in range(L):
-                        transfer_lps = lens(h, idx=i).log_softmax(dim=-1)
-                        transfer_ces[i, j] = th.nn.functional.cross_entropy(
-                            shift_preds(transfer_lps, shift).flatten(0, 1),
-                            labels.flatten(),
-                        )
-                        transfer_kls[i, j] = th.sum(
-                            lens_probs * (lens_lps - transfer_lps), dim=-1
-                        ).mean()
-
-            final_logit_stats.update(final_lps, assume_normalized=True)
+                    batch_output[lens_type]["entropy"][name] = th.sum(
+                        -lens_probs * lens_lps, dim=-1
+                    )
+                    batch_output[lens_type]["kl"][name] = th.sum(
+                        final_probs * (final_lps - lens_lps), dim=-1
+                    )
 
             batch_output["baseline_ce"]["final"] = th.nn.functional.cross_entropy(
                 shift_preds(final_lps, shift).flatten(0, 1),
@@ -189,12 +189,7 @@ class Eval:
                 -final_probs * final_lps, dim=-1
             )
             batches.append(pytree_map(th.mean, batch_output))  # type: ignore[arg-type]
-            transfer_batches.append(
-                {
-                    "transfer_ce": transfer_ces,
-                    "transfer_kl": transfer_kls,
-                }
-            )
+
             # Keep the processes synced
             self.dist.barrier()
 
@@ -210,21 +205,7 @@ class Eval:
         assert isinstance(batches, dict)
 
         if self.dist.primary:
-            np.savez_compressed(root_dir / "batches.npz", **batches)
-            np.savez(root_dir / "aggregate_metrics.npz", **agg)
-
-        if self.transfer:
-            agg_transfer = pytree_map(
-                lambda x: nats_to_bpb * x.mean(0), pytree_stack(transfer_batches)
+            np.savez_compressed(
+                root_dir / "batches.npz", **flatten(batches, reducer="dot")
             )
-            agg_transfer = pytree_map(lambda x: maybe_all_reduce(x), agg_transfer)
-            agg_transfer = pytree_map(lambda x: x.cpu().numpy(), agg_transfer)
-
-            if self.dist.primary:
-                th.save(agg_transfer, root_dir / "aggregate_transfer_metrics.npz")
-
-        for stats in lens_statistics:
-            stats.all_reduce_()
-
-        if self.dist.primary:
-            th.save(lens_statistics, root_dir / "lens_logit_stats.pt")
+            np.savez(root_dir / "aggregate_metrics.npz", **flatten(agg, reducer="dot"))
