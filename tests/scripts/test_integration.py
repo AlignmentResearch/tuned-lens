0506b6a467f43ef9c6781ac27a68c37ec99ac960
Lev McKinney <levmckinney@gmail.com>
2023-08-29 16:03:45 -0400

    added value error when insufficient training data is provided

--- tests/scripts/test_integration.py
+++ tests/scripts/test_integration.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import pytest
+
 from tuned_lens.__main__ import main
 
 
@@ -36,3 +38,20 @@ def test_train_subcommand(
     assert Path(tmp_path, "checkpoints/snapshot_2.pth").exists()
     assert Path(tmp_path, "config.json").exists()
     assert Path(tmp_path, "params.pt").exists()
+
+
+def test_train_subcommand_fails_when_not_enough_data_given(
+    text_dataset_path: Path, gpt2_tiny_random_model_local_path: Path, tmp_path: Path
+):
+    args = (
+        f"--log_level DEBUG train --data.name {text_dataset_path}"
+        f" --model.name {gpt2_tiny_random_model_local_path}"
+        " --max_length 128"
+        " --tokens_per_step 256"
+        " --num_steps 100000"
+        " --checkpoint_freq 2"
+        f" --output {tmp_path}"
+    )
+    args = args.split()
+    with pytest.raises(ValueError, match="Can only take"):
+        main(args)
