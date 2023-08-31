960f3388b1f8668efd6c9ac1727baa551be8a0e0
Lev McKinney <levmckinney@gmail.com>
2023-08-31 17:27:22 -0400

    Better errors for when lens loading fails (#118)
    * better errors for when lens loading fails
    
    * Added revision

--- tests/test_load_artifact.py
+++ tests/test_load_artifact.py
@@ -1,5 +1,23 @@
-from tuned_lens.load_artifacts import load_lens_artifacts
+import pytest
+
+from tuned_lens.load_artifacts import available_lens_artifacts, load_lens_artifacts
 
 
 def test_load_lens_artifact_smoke():
     load_lens_artifacts("gpt2", "AlignmentResearch/tuned-lens")
+
+
+def test_load_lens_artifact_raises_smoke():
+    with pytest.raises(ValueError, match="Could not find lens at the specified"):
+        load_lens_artifacts("sia23s3asdr", "AlignmentResearch/tuned-lens")
+
+
+def test_list_available_lens_artifacts_smoke():
+    artifacts = available_lens_artifacts("AlignmentResearch/tuned-lens", "space")
+    assert len(artifacts) > 0
+    artifacts = available_lens_artifacts(
+        "AlignmentResearch/tuned-lens",
+        "space",
+        "revision_does_not_exist",
+    )
+    assert len(artifacts) == 0
